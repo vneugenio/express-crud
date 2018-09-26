96d50b6a2ee24d806bfb048918c2773c7623a2a8
bolts <voltaire.eugenio@gmail.com>
2018-09-26 11:17:18 +0800

    validation

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,65 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
 const bcrypt = require('bcrypt');
+const path = require('path');
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(expressValidator());
 
 
+// home / login page
+router.get('/', function(req, res) {
+    // res.json({message: 'Welcome'});
+    res.render('index', {
+        title:'Welcome'
+    });
+});
+
+
+// Register process
+router.post('/register', function(req, res, next) {
+
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    req.checkBody(username,'Username is required!').notEmpty();
+    req.checkBody(username, 'Username should be at least 5 characters').isLength({min:5, max: 5});
+
+    req.checkBody(email, 'Email is required').notEmpty();
+    req.checkBody(email, 'Pleased provide valid email').isEmail();
+    req.checkBody(password, 'Password should be at least 5 characters').isLength({min:5, max: 5});
+
+    let validation = req.validationErrors();
+
+    // Check last array in validation if undefined for errors
+    if(validation[validation.length-1].value === undefined){
+
+        // Check if username and email is unique in the collection
+        User.find({email: email}, function (err, docs) {
+            if(!docs.length) {
+                function NewUser(username, email, password) {
+                    this.username = username;
+                    this.email = email;
+                    this.password = bcrypt.hashSync(password, 10);
+                }
+
+                let newUser = new NewUser(username, email, password);
+
+                User.create(newUser).then(user => {
+                    res.send(user);
+                }).catch(next);
+            } else {
+                res.send('Email already exists');
+
+            }
+        });
+    } else {
+        res.send('Please provide valid details!');
+    }
+});
 
 // get list of all entries from db
 router.get('/users', (req, res, next) => {
@@ -17,10 +74,10 @@ router.get('/users', (req, res, next) => {
 router.get('/users/:userId', (req, res, next) => {
     User.findOne({_id: req.params.userId}).then(user => {
         res.send(user);
-    });
+    }).catch(next);
 });
 
-//Add a new entry
+//Add a new entry --- for testing on postman
 router.post('/users', (req, res, next) => {
 
     function NewUser(username, email, password) {
